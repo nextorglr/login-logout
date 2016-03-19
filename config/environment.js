327cb6f17a66eb5324484ccc88e1ec67983253ec
Adam <nextorglr@gmail.com>
2016-03-19 15:25:19 +0100

    added tore adapter

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,11 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'login-logout',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://login-logout.firebaseio.com/',
+    torii: {
+      sessionServiceName: 'session'
+    },
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
