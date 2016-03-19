4ff6ab432e366c9e9a38e4bceb663d088d1e05b8
Benedikt Dichgans <dichgans@eundd.com>
2016-03-19 15:28:45 +0100

    Register Template

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		registerUser() {
+			var ref = new Firebase(env.firebase);
+			ref.createUser({
+				email    : "bobtony@firebase.com",
+				password : "correcthorsebatterystaple"
+			}, function(error, userData) {
+				if (error) {
+					console.log("Error creating user:", error);
+				} else {
+					console.log("Successfully created user account with uid:", userData.uid);
+				}
+			});
+		}
+	}
 });
